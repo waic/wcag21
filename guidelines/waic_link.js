e1e1e0f10b92a5de44e0503af0a09dd58f9f6c20
ota <bakera@star.email.ne.jp>
2020-02-25 20:27:17 +0900

    リンクの訳注を書き換えるようにした

--- guidelines/waic_link.js
+++ guidelines/waic_link.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("body>div:first-child>p:first-child").after("<p>この文書内にあるリンクのうち、「Understanding WCAG 2.1」「Techniques for WCAG 2.1」「How to Meet WCAG 2.1」へのリンクについては、WAIC の公開する日本語版にリンク先を追加しています。WAIC の日本語訳は、 W3C の公開する英語版より内容が古い可能性がありますのでご注意ください。</p>");
+	$("aside.trnote>p:last-child").text("この文書内にあるリンクのうち、「Understanding WCAG 2.1」へのリンクについては、WAIC の公開する日本語版にリンク先を追加しています。WAIC の日本語訳は、 W3C の公開する英語版より内容が古い可能性がありますのでご注意ください。");
 	$("a[href*=https\\:\\/\\/www\\.w3\\.org\\/WAI\\/WCAG21]").each(function(){		
 		if( $(this).attr('href').indexOf("Understanding")!=-1 ){
 			var url = $(this).attr('href').replace('\/\/www.w3.org\/WAI\/WCAG21\/Understanding','//waic.jp/docs/WCAG21/Understanding');
