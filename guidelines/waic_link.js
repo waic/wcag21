7c97c873c5222f8bf480349c7a03db098569cc71
ota <bakera@star.email.ne.jp>
2020-02-26 13:51:51 +0900

    脱jQueryのテスト

--- guidelines/waic_link.js
+++ guidelines/waic_link.js
@@ -1,20 +1,22 @@
-$(function(){
-	$("aside.trnote>p:last-child").text("この文書内にあるリンクのうち、「Understanding WCAG 2.1」へのリンクについては、WAIC の公開する日本語版にリンク先を追加しています。WAIC の日本語訳は、 W3C の公開する英語版より内容が古い可能性がありますのでご注意ください。");
-	$("a[href*=https\\:\\/\\/www\\.w3\\.org\\/WAI\\/WCAG21]").each(function(){		
-		if( $(this).attr('href').indexOf("Understanding")!=-1 ){
-			var url = $(this).attr('href').replace('\/\/www.w3.org\/WAI\/WCAG21\/Understanding','//waic.jp/docs/WCAG21/Understanding');
-			var text = $(this).text();
-			$(this).after(" <a href='"+url+"' title='"+text+"の日本語訳'>[日本語訳]</a>");
+const trNote = "この文書内にあるリンクのうち、「Understanding WCAG 2.1」へのリンクについては、WAIC の公開する日本語版にリンク先を追加しています。WAIC の日本語訳は、 W3C の公開する英語版より内容が古い可能性がありますのでご注意ください。";
+const jaLinkText = '[日本語訳]';
+const jaLinkTitleSuffix = 'の日本語訳';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const lastTrNote = document.querySelector("aside.trnote>p:last-child");
+	if(lastTrNote) lastTrNote.textContent = trNote;
+
+	const w3cDocumentAnchors = document.querySelectorAll("a[href*=https\\:\\/\\/www\\.w3\\.org\\/WAI\\/WCAG21]")
+	w3cDocumentAnchors.forEach((anchor, index)=>{
+		const href = anchor.getAttribute('href');
+		if(href.indexOf("Understanding")!=-1 ){
+			const jaLinkUrl = href.replace('\/\/www.w3.org\/WAI\/WCAG21\/Understanding','//waic.jp/docs/WCAG21/Understanding');
+			const jaLinkTitle = '"' + anchor.textContent + '"' + jaLinkTitleSuffix;
+			const jaAnchor = document.createElement('a');
+			jaAnchor.setAttribute('href', jaLinkUrl);
+			jaAnchor.setAttribute('title', jaLinkTitle);
+			jaAnchor.textContent = jaLinkText;
+			anchor.parentNode.insertBefore(jaAnchor,anchor.nextSibling);
 		}		
-		// if( $(this).attr('href').indexOf("WCAG20-TECHS")!=-1 ){
-		// 	var url = $(this).attr('href').replace('\/\/www.w3.org\/TR\/WCAG20-TECHS','//waic.jp/docs/WCAG-TECHS');
-		// 	var text = $(this).text();
-		// 	$(this).after(" <a href='"+url+"' title='"+text+"の日本語訳'>[日本語訳]</a>");
-		// }
-		// if( $(this).attr('href').indexOf("quickref")!=-1 ){
-		// 	var url = $(this).attr('href').replace('\/\/www.w3.org\/WAI\/WCAG20','//waic.jp/docs/WCAG20');
-		// 	var text = $(this).text();
-		// 	$(this).after(" <a href='"+url+"' title='"+text+"の日本語訳'>[日本語訳]</a>");
-		// }
 	});
 });
